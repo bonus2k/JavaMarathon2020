bff296f537d0215833450e9288434cc369bf8446
Spartak Katvickii <bonus2@gmail.com>
2020-12-25 17:29:06 +0300

    Add tasks solution day2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,17 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int floor = scanner.nextInt();
+//        while ((floor = scanner.nextInt()) < 1) {
+//            System.out.printf("Ошибка ввода\n");
+//        }
+        if (floor < 1) System.out.printf("Ошибка ввода\n");
+        else if (floor >= 1 && floor <= 4) System.out.println("Малоэтажный дом");
+        else if (floor >= 5 && floor <= 8) System.out.println("Среднеэтажный дом");
+        else System.out.println("Многоэтажный дом");
     }
 }
