70a238a5db78c899a0665f70941cb0f80990f451
Spartak Katvickii <bonus2@gmail.com>
2020-12-26 14:36:57 +0300

    Add tasks solution day3

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,15 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            double divisible = scanner.nextDouble();
+            double divisor = scanner.nextDouble();
+            if (divisor == 0) break;
+            else System.out.println(divisible / divisor);
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,18 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            double divisible = scanner.nextDouble();
+            double divisor = scanner.nextDouble();
+            if (divisor==0){
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(divisible/divisor);
+        }
     }
 }
