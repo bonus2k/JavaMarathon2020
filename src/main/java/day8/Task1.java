41673d999cb71ebcc933cc25513632427477ad24
Spartak Katvickii <bonus2@gmail.com>
2021-01-02 11:16:58 +0300

    Add tasks solution day8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,23 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        Long start = System.currentTimeMillis();
+        String str = "";
+        for (int i = 0; i <= 20000; i++) {
+            str += " " + i;
+        }
+        Long stop = System.currentTimeMillis();
+        System.out.println("Время выполнения: " + (stop - start));
+        //System.out.println(str);
 
+        start = System.currentTimeMillis();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i <= 20000; i++) {
+            stringBuilder.append(" ");
+            stringBuilder.append(i);
+        }
+        stop = System.currentTimeMillis();
+        System.out.println("Время выполнения: " + (stop - start));
+        //System.out.println(stringBuilder);
     }
 }
