ca5cff0bc577b8036fdb55ea531f7cf1594164cd
Spartak Katvickii <bonus2@gmail.com>
2021-01-09 15:22:44 +0300

    Add tasks solution day15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,25 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File inFile = new File("src/main/resources/shoes.csv");
+        File outFile = new File("src/main/resources/missing_shoes.txt");
 
+        try (Scanner scanner = new Scanner(inFile);
+             PrintWriter printWriter = new PrintWriter(outFile)) {
+            while (scanner.hasNext()) {
+                String str = scanner.nextLine();
+                if (str.matches(".+;0$")) {
+                    printWriter.println(str);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
