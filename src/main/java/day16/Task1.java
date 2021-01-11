7e733e7129520b0bc7ae937c6bf335724900d50c
Spartak Katvickii <bonus2@gmail.com>
2021-01-11 07:18:14 +0300

    Add task solution day16

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,28 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        printResult(new File("numbers.txt"));
+    }
 
+    public static void printResult(File file) {
+        String input = "";
+        double countSum = 0.0;
+        try (Scanner scanner = new Scanner(file)) {
+            input = scanner.nextLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String[] arrayInput = input.split(" ");
+        for (String digit : arrayInput) {
+            countSum += Double.parseDouble(digit);
+        }
+        countSum /= arrayInput.length;
+        System.out.printf(countSum + " --> %.3f", countSum);
     }
 }
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,74 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
 public class Task2 {
     public static void main(String[] args) {
+        File file1 = new File("file1.txt");
+        File file2 = new File("file2.txt");
+        generateIntegerInFile(file1);
+        countingTwentyNumbersFromFile(file1, file2);
+        printResult(file2);
 
+    }
+
+    public static void printResult(File file) {
+        Integer sum = null;
+        try (Scanner scanner = new Scanner(file)) {
+            sum = (int) Arrays.stream(scanner.nextLine().split(" "))
+                    .mapToDouble(Double::parseDouble)
+                    .sum();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(sum);
+    }
+
+    public static void generateIntegerInFile(File file) {
+        Random random = new Random();
+        try (PrintWriter printWriter = new PrintWriter(file)) {
+            StringJoiner sj = new StringJoiner(" ");
+            for (int i = 0; i < 1000; i++) {
+                sj.add(random.nextInt(100) + "");
+            }
+            printWriter.print(sj);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void countingTwentyNumbersFromFile(File file1, File file2) {
+        String[] allNumbers = null;
+        StringJoiner sj = new StringJoiner(" ");
+        int count = 0;
+        double sum = 0;
+        try (Scanner scanner = new Scanner(file1)) {
+            allNumbers = scanner.nextLine().split(" ");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for (String numbers : allNumbers) {
+            if (count < 20) {
+                sum += Integer.parseInt(numbers);
+                count++;
+            } else {
+                sj.add(sum / 20 + "");
+                count = 0;
+                sum = 0;
+            }
+        }
+
+        try (PrintWriter printWriter = new PrintWriter(file2)) {
+            printWriter.print(sj);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
