daca86a5264264df446f1f49a3b8a9beaffcbf8f
Spartak Katvickii <bonus2@gmail.com>
2021-01-11 07:27:40 +0300

    Fix test

--- src/test/java/Tests/day18/Day18Task1Test.java
+++ src/test/java/Tests/day18/Day18Task1Test.java
@@ -8,21 +8,21 @@
 //
 //    @Test
 //    void recursion1() {
-//        int[] numbers.txt = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+//        int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
 //        Assertions.assertEquals(65346, Task1.recursionSum(numbers.txt, 0),
 //                "[1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311]");
 //    }
 //
 //    @Test
 //    void recursion2() {
-//        int[] numbers.txt = {402, -50, -249, 215, 665, 311};
+//        int[] numbers = {402, -50, -249, 215, 665, 311};
 //        Assertions.assertEquals(1294, Task1.recursionSum(numbers.txt, 0),
 //                "[402, -50, -249, 215, 665, 311]");
 //    }
 //
 //    @Test
 //    void recursion3() {
-//        int[] numbers.txt = {-402, -50, -249, -15, -665, -311};
+//        int[] numbers = {-402, -50, -249, -15, -665, -311};
 //        Assertions.assertEquals(-1692, Task1.recursionSum(numbers.txt, 0),
 //                "[-402, -50, -249, -15, -665, -311]");
 //    }
